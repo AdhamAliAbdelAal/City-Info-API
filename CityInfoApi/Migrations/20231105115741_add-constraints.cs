ce2ef167cf3f55291680920e6362a46cf8b5f438
AdhamAliAbdelAal <adhamali20012015@gmail.com>
2023-11-05 14:27:12 +0200

    Add unstaged files

---
+++ CityInfoApi/Migrations/20231105115741_add-constraints.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CityInfoApi.Migrations
+{
+    public partial class addconstraints : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
